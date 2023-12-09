bcff84317e63ea1e739fc5bbf5e5354d356d529e
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-12-10 02:10:59 +0400

    chore: setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./src/tests/setupTests.ts'],
+    coverage: {
+      provider: 'v8',
+      all: true,
+    },
+    exclude: ['node_modules'],
   },
 });
