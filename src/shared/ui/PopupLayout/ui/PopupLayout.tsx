3b1eee3c2af1042ba82c0519b3fedf892a1606c2
Romanenkov Dmitriy <215rd@mail.ru>
2023-12-17 00:11:18 +0300

    feat: popup close icon

--- src/shared/ui/PopupLayout/ui/PopupLayout.tsx
+++ src/shared/ui/PopupLayout/ui/PopupLayout.tsx
@@ -28,7 +28,12 @@ export const PopupLayout = ({ children, isShown }: PopupLayoutType) => {
       role="button"
       tabIndex={0}
     >
-      {children}
+      <div className="relative">
+        <div className="absolute transition-opacity duration-300 top-5 right-5 text-white opacity-70 hover:opacity-100">
+          &#9587;
+        </div>
+        {children}
+      </div>
     </div>
   );
 };
