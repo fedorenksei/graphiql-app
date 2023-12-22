4084711ca4a27ee6549640171e95dac85b06279f
Romanenkov Dmitriy <215rd@mail.ru>
2023-12-22 22:42:15 +0300

    feat: url redux slice

---
+++ src/app/model/store/slices/urlSlice/index.ts
@@ -0,0 +1,20 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { UrlType } from './types';
+
+const initialState: UrlType = {
+  baseUrl: '',
+};
+
+const { reducer, actions } = createSlice({
+  name: 'urlSlice',
+  initialState,
+  reducers: {
+    setUrl: (state, { payload: { baseUrl } }: PayloadAction<UrlType>) => ({
+      ...state,
+      baseUrl,
+    }),
+  },
+});
+
+export const { setUrl } = actions;
+export default reducer;
