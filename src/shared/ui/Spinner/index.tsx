5bdd1a4b24d9de2964c59bc3a0a2542869b347e5
Romanenkov Dmitriy <215rd@mail.ru>
2023-12-28 22:59:42 +0300

    feat: add spinner

---
+++ src/shared/ui/Spinner/index.tsx
@@ -0,0 +1,10 @@
+import { Spinner } from '@nextui-org/react';
+
+export const RootSpinner = () => (
+  <div className="w-full h-full flex justify-center items-center pr-10">
+    <Spinner
+      color="warning"
+      size="lg"
+    />
+  </div>
+);
