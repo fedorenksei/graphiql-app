cc989e7f2c2d0a5e462bfce430ee92006df8d5e4
Romanenkov Dmitriy <215rd@mail.ru>
2023-12-29 19:58:07 +0300

    feat: doc for fields

--- src/shared/types/schema.ts
+++ src/shared/types/schema.ts
@@ -1,10 +1,26 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 export type DefaultSchemaType = {
   name: string;
 };
 
 export type DefaultSchemaReq = {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   __schema: {
     types: DefaultSchemaType[];
+  };
+};
+
+export type DefaultFieldsType = {
+  name: string;
+  description: string;
+  type: {
+    kind: string;
+    name: string;
+  };
+};
+
+export type DefaultFieldsReq = {
+  __type: {
+    fields: DefaultFieldsType[];
+    name: string;
   };
 };
