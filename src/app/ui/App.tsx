87b1ee72816184d6d0e3e2890760bc74c21d6417
Romanenkov Dmitriy <215rd@mail.ru>
2023-12-30 15:04:10 +0300

    feat: HAPPY NEW YEAR!

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -4,11 +4,13 @@ import { router } from '../model/router';
 import { store } from '../model/store/store';
 import './App.css';
 import '../model/firebase/firebase';
+import { SnowBlock } from './snowflakes/Snowflakes';
 
 export const App = () => {
   return (
     <Provider store={store}>
       <RouterProvider router={router} />
+      <SnowBlock />
     </Provider>
   );
 };
