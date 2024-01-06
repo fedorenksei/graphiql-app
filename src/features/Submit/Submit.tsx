0b5a219c513face7003597a367a81785df03f4f2
Aleksei Fedorenko <fedorenksei@gmail.com>
2024-01-06 11:20:46 +0400

    feat: add loading and disabled states to submit button

--- src/features/Submit/Submit.tsx
+++ src/features/Submit/Submit.tsx
@@ -2,27 +2,27 @@ import { setError, setResponse } from '@/app/model/store/slices/responseSlice';
 import { AppApi } from '@/shared/api';
 import { useAppDispatch, useAppSelector } from '@/shared/hooks/hooks';
 import { Button } from '@nextui-org/button';
+import { useState } from 'react';
 
 export default function Submit() {
   const dispatch = useAppDispatch();
   const baseUrl = useAppSelector((state) => state.urlReducer.baseUrl);
   const query = useAppSelector((state) => state.urlReducer.query);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const onClick = () => {
+  const onClick = async () => {
     if (!baseUrl || !query) return;
-    const fetchData = async () => {
+    setIsLoading(true);
+    try {
       const api = AppApi.getInstance();
-      try {
-        const resp = await api.getResponseForQuery(query);
-        const value = JSON.stringify(resp);
-        dispatch(setResponse(value));
-        dispatch(setError(''));
-      } catch {
-        dispatch(setError('Something went wrong'));
-        dispatch(setResponse(''));
-      }
-    };
-    fetchData();
+      const resp = await api.getResponseForQuery(query);
+      dispatch(setResponse(JSON.stringify(resp)));
+      dispatch(setError(''));
+    } catch {
+      dispatch(setError('Something went wrong'));
+      dispatch(setResponse(''));
+    }
+    setIsLoading(false);
   };
 
   return (
@@ -30,6 +30,8 @@ export default function Submit() {
       type="button"
       className="justify-self-end"
       onClick={onClick}
+      isLoading={isLoading}
+      isDisabled={!baseUrl || !query}
     >
       Submit
     </Button>
