fe1d06f6e700ae791539cc50d839f9f8758d8aba
Aleksei Fedorenko <fedorenksei@gmail.com>
2024-01-07 18:45:45 +0400

    feat: implement request headers

---
+++ src/features/Headers/model/transform.ts
@@ -0,0 +1,11 @@
+import { UiHeader } from './types';
+
+export const transformHeaders = (
+  uiHeaders: UiHeader[],
+): Record<string, string> => {
+  return uiHeaders.reduce<Record<string, string>>((acc, curr) => {
+    // eslint-disable-next-line no-param-reassign
+    if (curr.name && curr.value) acc[curr.name] = curr.value;
+    return acc;
+  }, {});
+};
